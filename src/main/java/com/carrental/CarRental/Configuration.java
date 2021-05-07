207fa5a39e3d5dec1cc443277183f69d040588b2
Michal Kozik <michal.kozikowski99@gmail.com>
2021-05-07 19:49:43 +0200

    Skonfigurowanie projektu.

---
+++ src/main/java/com/carrental/CarRental/Configuration.java
@@ -0,0 +1,17 @@
+package com.carrental.CarRental;
+
+import javax.annotation.sql.DataSourceDefinition;
+import javax.ejb.Singleton;
+
+@DataSourceDefinition(
+        name = "java:global/JsfCrudDemoDataSource",
+        className = "org.h2.jdbcx.JdbcDataSource",
+        url = "jdbc:h2:mem:jsfcruddemo;DB_CLOSE_DELAY=-1",
+        minPoolSize = 1,
+        initialPoolSize = 1,
+        user = "sa",
+        password = ""
+)
+@Singleton
+public class Configuration {
+}
